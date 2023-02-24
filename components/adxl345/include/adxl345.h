f219f6106b0c3afd98ed3f2a004ef5ab8a6d9384
kriraskov <kristoffer.raskov@gmail.com>
2023-02-24 12:13:10 +0100

    Cleanup.

--- components/adxl345/include/adxl345.h
+++ components/adxl345/include/adxl345.h
@@ -1,5 +1,5 @@
-#ifndef ESP32_FLAG_ACCEL_ACCELEROMETER_H
-#define ESP32_FLAG_ACCEL_ACCELEROMETER_H
+#ifndef ESP32_FLAG_ACCEL_ADXL345_H
+#define ESP32_FLAG_ACCEL_ADXL345_H
 
 #define ADXL345_SENSOR_ADDR                 0x53        /*!< Slave address of the ADXL345 sensor. */
 
@@ -12,8 +12,8 @@ esp_err_t adxl345_register_read(uint8_t reg_addr, uint8_t *data, size_t len);
 
 esp_err_t adxl345_register_write_byte(uint8_t reg_addr, uint8_t data);
 
-esp_err_t adxl345_i2c_slave_init(void);
+esp_err_t adxl345_init(void);
 
 esp_err_t adxl345_read_data(int16_t *ax, int16_t *ay, int16_t *az);
 
-#endif //ESP32_FLAG_ACCEL_ACCELEROMETER_H
+#endif //ESP32_FLAG_ACCEL_ADXL345_H
